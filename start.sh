549d21929f875ce96499dee1ff9733a168feca7c
Pedro Mossi <pedro.mossi@senior.com.br>
2022-12-15 16:07:52 -0300

    0.4.0

--- start.sh
+++ start.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 # Change to the fastapi directory
-cd ./fastapi
+
+kill -9 $(lsof -t -i:5000) $(lsof -t -i:8000)
+
+cd /home/api/API/fastapi
 
 # Start fastapi
 uvicorn main:app --reload &
 
 # Change to the flask directory
-cd ..
-cd ./flask
+cd /
+cd /home/api/API/flask
 # start flask
-flask --app main_flask --debug run &
+flask --app main_flask --debug run --host=0.0.0.0 &
 
 # Wait for both processes to finish
 wait
+
+trap "kill $(lsof -t -i:5000) $(lsof -t -i:8000)" SIGINT SIGTERM
